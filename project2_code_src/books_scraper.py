a0bcd3609227070e2677fa8834bf3aad321392ec
Michie Adams <michiemadams@outlook.com>
2025-06-25 20:06:41 -0700

    Added code to scrape Poetry category and create csv file.

--- project2_code_src/books_scraper.py
+++ project2_code_src/books_scraper.py
@@ -3,82 +3,112 @@ from bs4 import BeautifulSoup
 import csv
 
 site_url = "http://books.toscrape.com/" #captures Books to Scrape url
-product_page_url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html" # url for one book
-page = requests.get(product_page_url)
-soup = BeautifulSoup(page.content, "html.parser")
+product_category_url = "https://books.toscrape.com/catalogue/category/books/poetry_23/index.html" # url for poetry books
+poetry_page = requests.get(product_category_url)
+poetry_soup = BeautifulSoup(poetry_page.content, "html.parser")
 
-# Creating a dictionary for the csv and adding the first pair
-alita_data = {'product_page_url' : product_page_url}
+# Build list of product page urls.
+product_page_url = []
+book_parent = poetry_soup.find_all("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3") #Finds url parent
 
-# Scraping the table at the bottom of the page for UPC, price w and w/o tax, and availability
-info_tbl = soup.find("table", class_="table")
-info_rows = info_tbl.find_all("tr")
+for book in book_parent:
+    url_tag = book.find("h3").find("a")
+    urls = url_tag["href"]
+    url = site_url + "catalogue/" + urls[9:]
+    product_page_url.append(url)
 
-# UPC
-universal_product_code = info_rows[0].find_all("td")[0].text.strip() #upc is in the first td of first row
-alita_data['universal_product_code'] = universal_product_code
-#print("UPC:", universal_product_code)
+def get_tbl_data(soup):
+    """Scrape table data from product page for
+    upc, price w & w/o taxes, and availability.
+    Clean up availability str to just numbers."""
+    info_tbl = soup.find("table", class_="table")
+    info_rows = info_tbl.find_all("tr")
+    universal_product_code = info_rows[0].find_all("td")[0].text.strip()
+    price_including_tax = info_rows[3].find_all("td")[0].text.strip()
+    price_excluding_tax = info_rows[2].find_all("td")[0].text.strip()
+    available_wText = info_rows[5].find_all("td")[0].text.strip()
+    for word in available_wText.split():
+        cleaned_word = word.strip("()")  # remove parentheses
+        if cleaned_word.isdigit():
+            quantity_available = cleaned_word
+            break
+    else:
+        quantity_available = "0"
+    return universal_product_code, price_including_tax, price_excluding_tax, quantity_available
 
-# Capture the title
-title_parent = soup.find("h1") # finds title which is listed near top under the body
-book_title = title_parent.text.strip() #strips white space
-alita_data['book_title'] = book_title
-#print("Book title:", book_title)
 
-# Price with tax
-price_including_tax = info_rows[3].find_all("td")[0].text.strip()
-alita_data['price_including_tax'] = price_including_tax
-#print("Price including tax:", price_including_tax)
+def get_title(soup):
+    """Scrape book title from product page."""
+    title_parent = soup.find("h1")
+    book_title = title_parent.text.strip()
+    return book_title
 
-# Price before tax
-price_excluding_tax = info_rows[2].find_all("td")[0].text.strip()
-alita_data['price_excluding_tax'] = price_excluding_tax
-#print("Price excluding tax:", price_excluding_tax)
+def description(soup):
+    prod_desc_parent = soup.find("article", class_="product_page")  # Identifies parent of description
+    prod_desc_next = prod_desc_parent.find_all("p")  # description listed under a <p> tag
+    product_description = prod_desc_next[3].text.strip()  # strips white space and captures 4th <p>
+    return product_description
 
-# Availability
-quantity_available = info_rows[5].find_all("td")[0].text.strip()
-quantity_available = quantity_available[10:13]
-alita_data['quantity_available'] = quantity_available
-#print("Quantity available:", quantity_available)
+def category(soup):
+    cat_parent = soup.find("ul", class_="breadcrumb")  # finds category which is a link on the page
+    cat_link = cat_parent.find_all("a")  # category listed under an <a> tag
+    category = cat_link[2].text.strip()  # strips white space and captures 3rd <a>
+    return category
 
-# Capture the description
-prod_desc_parent = soup.find("article", class_="product_page") #Identifies parent of description
-prod_desc_next = prod_desc_parent.find_all("p") # description listed under a <p> tag
-product_description = prod_desc_next[3].text.strip() # strips white space and captures 4th <p>
-alita_data['product_description'] = product_description
-#print("Product description:", product_description)
+def review(soup):
+    rtg_parent = soup.find("p", class_="star-rating")
+    rtg_tags = rtg_parent["class"]
+    review_rating = rtg_tags[1]
+    return review_rating
 
-# Capture the category
-cat_parent = soup.find("ul", class_="breadcrumb") # finds category which is a link on the page
-cat_link = cat_parent.find_all("a") #category listed under an <a> tag
-category = cat_link[2].text.strip() #strips white space and captures 3rd <a>
-alita_data['category'] = category
-#print("Category:", category)
+def img_url(soup):
+    img_parent = soup.find("div", class_="item active")  # finds img parent class
+    img_tag = img_parent.find("img")  # finds img tag
+    img_src = img_tag["src"]  # identifies partial url in tag
+    img_src = img_src[6:]  # removes '../../' from partial html url
+    image_url = site_url + img_src  # builds full url link
+    return image_url
 
-# Review rating
-rtg_parent = soup.find("p", class_="star-rating")
-rtg_tags =  rtg_parent["class"]
-review_rating = rtg_tags[1]
-alita_data['review_rating'] = review_rating
-#print("Review rating:", review_rating)
+poetry_books_data = [] # list for all poetry book dictionaries
 
-# Product image url
-img_parent = soup.find("div", class_="item active") #finds img parent class
-img_tag = img_parent.find("img") # finds img tag
-img_src = img_tag["src"] #identifies partial url in tag
-img_src = img_src[6:] # removes '../../' from partial html url
-image_url = site_url + img_src # builds full url link
-alita_data['image_url'] = image_url
-#print("Image url:", image_url)
+for url in product_page_url:
+    book_dict = {}  # creating the dictionary base for each book
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    get_tbl_data(soup)
+    get_title(soup)
+    description(soup)
+    category(soup)
+    review(soup)
+    img_url(soup)
 
-# print(alita_data)
+    book_dict['product_page_url'] = url
+    book_dict['book_title'] = get_title(soup)
+    book_dict['universal_product_code'] = get_tbl_data(soup)[0]
+    book_dict['price_including_tax'] = get_tbl_data(soup)[1]
+    book_dict['price_excluding_tax'] = get_tbl_data(soup)[2]
+    book_dict['availability'] = get_tbl_data(soup)[3]
+    book_dict['description'] = description(soup)
+    book_dict['category'] = category(soup)
+    book_dict['review'] = review(soup)
+    book_dict['img_url'] = img_url(soup)
+
+    poetry_books_data.append(book_dict) # append active iteration of book_dict to list of poetry dictionaries
+
 
 # build csv output
-with open('alita.csv', mode='w', newline="") as csvfile:
-    fieldnames = alita_data.keys()
+with open('poetry.csv', mode='w', newline="") as csvfile:
+    fieldnames = ['product_page_url', 'book_title', 'universal_product_code', 'price_including_tax', 'price_excluding_tax', 'availability', 'description', 'category', 'review', 'img_url']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     writer.writeheader()
-    writer.writerows([alita_data])
+    writer.writerows(poetry_books_data)
+# build csv output
+# with open('alita.csv', mode='w', newline="") as csvfile:
+#     fieldnames = alita_data.keys()
+#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#
+#     writer.writeheader()
+#     writer.writerows([alita_data])
 
 
